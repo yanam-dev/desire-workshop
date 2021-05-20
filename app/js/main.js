9178b071b72fc7faab4d004058697ad08c569a57
yanam-dev <yamusjko@gmail.com>
2021-05-21 00:59:29 +0200

    added top slider main page

--- app/js/main.js
+++ app/js/main.js
@@ -6,7 +6,15 @@ $(function(){
     $('.rightside-menu__close').on('click', function() {
         $('.rightside-menu').addClass('rightside-menu--close');
     });
+    // $('.rightside-menu').toggleClass();
 
 //SLIDER main//
+    $('.top__slider').slick({
+        dots: true,
+        arrows: false,
+        fade: true,
+        autoplay: true,
+        autoplaySpeed: 3000
+    });
 
 })
